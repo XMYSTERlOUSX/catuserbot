033a2de091cf260d9bb8a21a598588d7c9c0392e
꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-04-10 03:38:50 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -108,23 +108,23 @@ async def on_afk(event):
         if AFK_.afk_type == "text":
             if AFK_.msg_link and AFK_.reason:
                 message_to_reply = (
-                    f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.**🥺 \n__If you see my master online it is me the userbot.😞__\n**AFK Since** : {endtime} __(The offline time rests when the server restarts)__ \n**Reason :** `{AFK_.reason}`"
+                    f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.**🥺 \n__If you see my master online it is me the userbot.😞__\n \n**AFK(Away From Keyboard) Since** : {endtime} \n__(The offline time rests when the server restarts)__ \n \n**Reason :** `{AFK_.reason}`"
                 )
             elif AFK_.reason:
                 message_to_reply = (
-                    f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n**AFK Since** : {endtime} __(The offline time rests when the server restarts)__ \n**Reason :** `{AFK_.reason}`"
+                    f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n \n**AFK(Away From Keyboard) Since** : {endtime} \n__(The offline time rests when the server restarts)__ \n \n**Reason :** `{AFK_.reason}`"
                 )
             else:
-                message_to_reply = f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n**AFK Since** : {endtime} __(The offline time rests when the server restarts)__ \n**Reason** :<a href = 'https://telegra.ph/Why-do-you-see-me-online-04-09'>**Read my bio**</a>"
+                message_to_reply = f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n \n**AFK(Away From Keyboard) Since** : {endtime} \n__(The offline time rests when the server restarts)__ \n \n**Reason** :https://telegra.ph/Why-am-I-not-online-any-more-04-09>"
             if event.chat_id not in Config.UB_BLACK_LIST_CHAT:
                 msg = await event.reply(message_to_reply)
         elif AFK_.afk_type == "media":
             if AFK_.reason:
                 message_to_reply = (
-                    f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n**AFK Since** : {endtime} __(The offline time rests when the server restarts)__ \n**Reason :** `{AFK_.reason}`"
+                    f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n \n**AFK(Away From Keyboard) Since** : {endtime} \n__(The offline time rests when the server restarts)__ \n \n**Reason :** `{AFK_.reason}`"
                 )
             else:
-                message_to_reply = f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n**AFK Since** : {endtime} __(The offline time rests when the server restarts)__ \n**Reason** :<a href = 'https://telegra.ph/Why-do-you-see-me-online-04-09'>**Read my bio**</a>"
+                message_to_reply = f"**꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ is not online anymore.🥺** \n__If you see my master online it is me the userbot.😞__\n \n**AFK(Away From Keyboard) Since** : {endtime} \n__(The offline time rests when the server restarts)__ \n \n**Reason** :https://telegra.ph/Why-am-I-not-online-any-more-04-09>"
             if event.chat_id not in Config.UB_BLACK_LIST_CHAT:
                 msg = await event.reply(message_to_reply, file=AFK_.media_afk.media)
         if event.chat_id in AFK_.last_afk_message:
